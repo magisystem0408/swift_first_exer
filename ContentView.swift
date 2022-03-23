94854e9a537322c76c7143cbefb6bc5849fe8492
magisystem0408 <masato.matsudo@gmail.com>
2022-03-23 10:56:24 +0900

    add image

--- ContentView.swift
+++ ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @State var favoritAnimal = ""
     var body: some View {
+        Image("test")
+            .resizable()
+            .frame(width: 200, height: 100, alignment: .center)
         //            onCommitでリターンした時に何かが発火する
         VStack{
             TextField("好きな動物を入力してください",text: $favoritAnimal,onCommit: {
